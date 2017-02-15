aa0885b25da0b45ecc7c006da18324a7041d4f56
ultrapowerAU <leichen070@gmail.com>
2017-02-15 10:42:19 +0800

    go基础学习-流程控制-2

--- src/github.com/CLgithub/1basis/2processcontrol/demo1.go
+++ src/github.com/CLgithub/1basis/2processcontrol/demo1.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math"
+	"runtime"
+	"time"
 )
 
 //测试for循环
@@ -54,7 +56,7 @@ func pow(a,b,c float64) float64 {
 	//这里开始就不能使用i了
 	return c
 }
-//练习
+//练习 用牛顿发算平方根
 func Sqrt(x float64) {
 	fmt.Printf("%g 的平方根是 %g \n", x,math.Sqrt(x))
 	z0:=1.0
@@ -77,9 +79,67 @@ func Sqrt(x float64) {
 		z1=z1 						//运输完后把运输后的赋给z1,准备下次运输
 	}
 }
+
+//测试switch
+func testSwitch1() {
+	switch os:=runtime.GOOS; os{
+		case "linux":
+			fmt.Printf(os)
+			fallthrough			//使用这个关键字后，执行完该分支后不会跳出，而会继续往下不判断条件直接执行下面的内容
+		case "darwin":
+			fmt.Printf("Mac os")
+		case "aa":
+			fmt.Println("aa")
+		default :
+			fmt.Printf("%g",os)
+	}	
+	fmt.Println("")
+}
+//测试switch
+func testSwitch2() {
+	fmt.Println("When's saturday?");	
+	today:=time.Now().Weekday()		//得到当前时间，得到当天星期几
+	// fmt.Println(today)				
+	// s:=time.Saturday
+	// fmt.Println(s)
+	switch time.Saturday{		// time里的常量
+		case today+0:
+			fmt.Println("today")
+		case today+1:
+			fmt.Println("Tomorrow")
+		case today+2:
+			fmt.Println("In tow days")
+		default:
+			fmt.Println("Too far away")
+	}
+	
+}
+//测试switch3,没有条件的switch，这种形式能将一长串if-then-else写得更加清晰
+func testSwitch3() {
+	t:=time.Now()	
+	switch{
+		case t.Hour()<12:
+			fmt.Println("morning")	
+		case t.Hour()<17:
+			fmt.Println("afternoon")
+		default:
+			fmt.Println("evening")
+	}
+}
+
+//测试defer		defer语句会将函数推迟到外层函数返回之后执行
+func testDefer1() {
+	defer fmt.Println("a")
+	fmt.Println("b")
+	for i := 0; i < 10; i++ {	//defer 是存储在一个栈中，先进后出
+		defer fmt.Println(i)			
+	}	
+}
 func main() {
 	// testFor2()	
 	// fmt.Println(fmt.Sprint('a'),fmt.Sprint("a"))
 	// testIf2()
-	Sqrt(4)
+	// Sqrt(4)
+	// testSwitch3()
+	testDefer1()
 }
